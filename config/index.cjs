a79d09367e01d652181d32fe0f13b936495b808e
bcc666 <108379279+bcc666@users.noreply.github.com>
2022-10-29 12:27:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '1089ca21775bbc9670d80838bc8b1cc2',
 
-  PROVINCE: '江苏',
-  CITY: '南京',
+  PROVINCE: '黑龙江',
+  CITY: '哈尔滨',
 
   USERS: [
     {
